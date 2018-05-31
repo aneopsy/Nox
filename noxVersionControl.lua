d0add2e767259f843debe3581fb54cef1e844664
THEIS <paultevatheis@gmail.com>
2018-05-31 19:18:00 +0200

    Update

--- noxVersionControl.lua
+++ noxVersionControl.lua
@@ -1,15 +1,4 @@
 Data = {
-    Dependencies = {
-        timez = {
-            Version = 1,
-        },
-        tracker = {
-            Version = 1,
-        },
-        menuLoad = {
-            Version = 1,
-        },
-    },
     Loader = {
         Version = 1,
     },
